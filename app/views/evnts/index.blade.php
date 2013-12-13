0fcc43c772c216d4570f8d4de8bdcf0150c8bf9c
Kim fraser <kimfraser@gmail.com>
2013-12-13 10:50:42 -0500

    adjust image height

--- app/views/evnts/index.blade.php
+++ app/views/evnts/index.blade.php
@@ -23,7 +23,7 @@
 					    <div class="thumbnail">
 					    	<br />
 					    	@if($e->flier)
-					    	<a href={{URL::route('evnts.show',$e->slug)}}><img src={{Cloudy::show($e->flier, array('width' => 300, 'height' => 200, 'crop' => 'fit', 'radius' => 0))}}></a>
+					    	<a href={{URL::route('evnts.show',$e->slug)}}><img src={{Cloudy::show($e->flier, array('width' => 300, 'height' => 180, 'crop' => 'fit', 'radius' => 0))}}></a>
 					    	@else
 							<img src="http://placehold.it/300x200">
 							@endif
