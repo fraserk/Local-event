c5584df861864006f35cf09e220d15002f230f29
Kim fraser <kimfraser@gmail.com>
2013-12-14 22:05:25 -0500

    add no fler image

--- app/views/evnts/index.blade.php
+++ app/views/evnts/index.blade.php
@@ -25,7 +25,7 @@
 					    	 @if($e->flier)
 					    	<a href={{URL::route('evnts.show',$e->slug)}}><img src={{Cloudy::show($e->flier, array('width' => 300, 'height' => 180, 'crop' => 'fit', 'radius' => 0))}}></a>
 					    	@else
-							<img src="http://placehold.it/300x200">
+							{{HTML::image('/img/nofler.png')}}
 							@endif
 					         
 					    
