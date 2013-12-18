80a3449da48af5f6b171ad627a27a2bce2f1ad43
Kim fraser <kimfraser@gmail.com>
2013-12-18 15:04:02 -0500

    remove newsletter

--- app/views/layout/template.blade.php
+++ app/views/layout/template.blade.php
@@ -89,13 +89,7 @@
      We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
 </style>
 <div id="mc_embed_signup">
-<form action="http://goingclubing.us1.list-manage.com/subscribe/post?u=348e9a6235a415d471b266c3a&amp;id=58036cca84" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-  <label for="mce-EMAIL">Subscribe to our mailing list</label>
-  <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
-    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-    <div style="position: absolute; left: -5000px;"><input type="text" name="b_348e9a6235a415d471b266c3a_58036cca84" value=""></div>
-  <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-</form>
+
 </div>
 
 <!--End mc_embed_signup-->
